5a426b27a19b73615186c78700e1c0342cb7b2cd
Ukezi Ebenezer <ebenezerukezie@gmail.com>
2025-06-22 10:08:37 +0100

    updated js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 AOS.init({duration:1000, once:true});
+
+const hamburger = document.getElementById('hamburger');
+const navBar = document.getElementById('navBar');
+
+hamburger.addEventListener("click", () => {
+    navBar.classList.toggle('active');
+})
+
+new Typed ("#typing", {
+    strings:['UI/UX Designer.', "Web Developer.", "Creative Thinker", "Stragetic Thinker"],
+    typeSpeed: 60, backSpeed: 30, backDelay: 1000, loop:true,
+});
